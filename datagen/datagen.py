db5e2364db0f9946acebe3e109a968e91fb99e3a
Afis KOUSSE <mahoutinafis@gmail.com>
2023-02-13 17:20:35 +0100

    get csv feature

--- datagen/datagen.py
+++ datagen/datagen.py
@@ -2,8 +2,10 @@
 Data gen module contains essential tools to gennerate
 data to csv format.
 """
-
 import csv
+import random
+import numpy
+
 
 def get_csv(rows, features={}, to_path="gen.csv"):
     """
@@ -14,10 +16,34 @@ def get_csv(rows, features={}, to_path="gen.csv"):
     if not cols:
         return
 
-    datas = [["ok" for i in range(cols)] for j in range(rows)]
+    datas = [[] for j in range(cols)]
+
+    idx = 0
+    for j in features.keys():
+        datas[idx] = __random_gen(rows, features[j])
+        idx = idx +1
+
+    datas = numpy.transpose(datas)
 
     with open(to_path, "w", encoding='UTF8' , newline='') as f:
         writer = csv.writer(f)
         writer.writerow(features.keys())
 
         writer.writerows(datas)
+
+
+def __random_gen(rows, feat):
+    """
+    Generate randomly data for column and return array
+    """
+    datas = ["" for j in range(rows)]
+    idx = 0
+    i = 0
+
+    for key in feat.keys():
+        for i in range(idx , idx + 1 + feat[key]):
+            if i < rows:
+                datas[i] = key
+        idx = i
+    random.shuffle(datas)
+    return datas
--- example.py
+++ example.py
@@ -1,9 +1,9 @@
 from datagen.datagen import get_csv
 
 features = {
-    "Gender": {"Male": 14, "Female": 60, "Other": 40},
+    "Gender": {"Male": 150, "Female": 300, "Other": 50},
     "Religion": {"christ": 41, "muslim": 102, "athe": 41},
     "Race": {"black": 50, "white": 24, "asian": 100}
 }
 
-get_csv(100, features)
+get_csv(500, features)
